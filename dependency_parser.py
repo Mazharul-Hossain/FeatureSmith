6579d2bb4dda7be25d1c2ef7c230d9c6394fdb70
mazhar69 <hossain.mazharul@gmail.com>
2017-08-20 20:38:45 +0600

    just starting

---
+++ dependency_parser.py
@@ -0,0 +1,5 @@
+# https://stackoverflow.com/q/29049974/6379197
+# https://stackoverflow.com/q/7443330/6379197
+# https://stackoverflow.com/q/29603056/6379197
+#https://www.quora.com/How-can-we-extract-the-main-verb-from-a-sentence-For-example-parrots-do-not-swim-Here-the-main-verb-is-swim-How-can-we-extract-that-by-language-processing-Are-there-any-known-algorithms-for-this-purpose
+
