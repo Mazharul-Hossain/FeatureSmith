f2e3419b3035cdacc3662c05db48f9b1364150e7
mazhar69 <hossain.mazharul@gmail.com>
2017-08-25 22:39:30 +0600

    test code

--- dependency_parser.py
+++ dependency_parser.py
@@ -1,4 +1,6 @@
-# https://stackoverflow.com/q/29049974/6379197
+#!/usr/bin/env python
+
+#  https://stackoverflow.com/q/29049974/6379197
 # https://stackoverflow.com/q/7443330/6379197
 # https://stackoverflow.com/q/29603056/6379197
 # https://www.quora.com/How-can-we-extract-the-main-verb-from-a-sentence-For-example-parrots-do-not-swim-Here-the-main-verb-is-swim-How-can-we-extract-that-by-language-processing-Are-there-any-known-algorithms-for-this-purpose
@@ -24,8 +26,6 @@ while attempts < 3:
         print json.dumps(parsed, indent=4, sort_keys=True)
 
         break
-    except urllib.URLError as e:
-        attempts += 1
-        print str(e)
     except Exception as e:
+        attempts += 1
         print str(e)
